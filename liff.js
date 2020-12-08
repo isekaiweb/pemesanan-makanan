35175928fcf50b6c7640a16967053f85dcc4d670
isekaiweb <cobasaja271@gmail.com>
2020-12-08 23:08:00 +0700

    penambahan tombol inc & dec

--- liff.js
+++ liff.js
@@ -1,40 +1,40 @@
-ambilApiLIFF("1655324717-zK2NJ5e3");
+// ambilApiLIFF("1655324717-zK2NJ5e3");
 
-function ambilApiLIFF(idLiff) {
-  liff
-    .init({
-      liffId: idLiff,
-    })
-    .then(() => {
-      mulaiAPP();
-    })
-    .catch(() => {
-      alert("Id LIFF Tidak ada");
-    });
-}
+// function ambilApiLIFF(idLiff) {
+//   liff
+//     .init({
+//       liffId: idLiff,
+//     })
+//     .then(() => {
+//       mulaiAPP();
+//     })
+//     .catch(() => {
+//       alert("Id LIFF Tidak ada");
+//     });
+// }
 
-function mulaiAPP() {
-  //cek apakah sudah login
-  if (!liff.isLoggedIn()) {
-    liff.login();
-  } else {
-    liff
-      .getProfile()
-      .then((profile) => {
-        profilContainer.innerHTML += ` <img src=${profile.pictureUrl}
-                                class="img-fluid rounded-circle"
-                                id="img-profil"
-                                alt="foto profil"
-                                />`;
-        profilContainer.innerHTML += `<p id="nama-profil">Hi, ${profile.displayName}</p>`;
-      })
-      .catch((err) => {
-        console.log("error", err);
-      });
-    removeClass(body, "d-none");
-  }
-}
+// function mulaiAPP() {
+//   //cek apakah sudah login
+//   if (!liff.isLoggedIn()) {
+//     liff.login();
+//   } else {
+//     liff
+//       .getProfile()
+//       .then((profile) => {
+//         profilContainer.innerHTML += ` <img src=${profile.pictureUrl}
+//                                 class="img-fluid rounded-circle"
+//                                 id="img-profil"
+//                                 alt="foto profil"
+//                                 />`;
+//         profilContainer.innerHTML += `<p id="nama-profil">Hi, ${profile.displayName}</p>`;
+//       })
+//       .catch((err) => {
+//         console.log("error", err);
+//       });
+//     removeClass(body, "d-none");
+//   }
+// }
 
-function removeClass(el, cls) {
-  el.classList.remove(cls);
-}
+// function removeClass(el, cls) {
+//   el.classList.remove(cls);
+// }
