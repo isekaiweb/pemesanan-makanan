cd719118581f2219659681d01632c83f69e12d24
isekaiweb <cobasaja271@gmail.com>
2020-12-08 17:44:39 +0700

    pencek login

---
+++ liff.js
@@ -0,0 +1,23 @@
+window.onload = () => {
+  ambilApiLIFF("1655324717-zK2NJ5e3");
+};
+
+function ambilApiLIFF(idLiff) {
+  liff
+    .init({
+      liffId: idLiff,
+    })
+    .then(() => {
+      mulaiAPP();
+    })
+    .catch(() => {
+      alert("Id LIFF Tidak ada");
+    });
+}
+
+function mulaiAPP() {
+  //cek apakah sudah login
+  if (!liff.isLoggedIn()) {
+    liff.login();
+  }
+}
