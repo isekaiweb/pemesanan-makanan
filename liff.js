fae152526437aaeacc954afb07e083bf396e001a
isekaiweb <cobasaja271@gmail.com>
2020-12-08 17:57:10 +0700

    membuat halam sembunyi jika belum login

--- liff.js
+++ liff.js
@@ -1,4 +1,5 @@
 window.onload = () => {
+  document.querySelector("body > .container").classList.add("d-none");
   ambilApiLIFF("1655324717-zK2NJ5e3");
 };
 
@@ -18,7 +19,8 @@ function ambilApiLIFF(idLiff) {
 function mulaiAPP() {
   //cek apakah sudah login
   if (!liff.isLoggedIn()) {
-    body.style.display = "none";
     liff.login();
+  } else {
+    document.querySelector("body > .container").classList.remove("d-none");
   }
 }
