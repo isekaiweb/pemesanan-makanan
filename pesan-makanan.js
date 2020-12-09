c4d415dee6f766aebc6c52852a22573c9335e6af
isekaiweb <cobasaja271@gmail.com>
2020-12-09 13:17:58 +0700

    fix button action quantity

--- pesan-makanan.js
+++ pesan-makanan.js
@@ -1,3 +1,29 @@
 const openBrowser = document.querySelector("#tombol-browser"),
   body = document.querySelector("body"),
   profilContainer = document.querySelector("#profil");
+
+document.querySelectorAll(".tombol-tambah").forEach((e) =>
+  e.addEventListener("click", function () {
+    changeDisplay(this.parentElement.children);
+    e.parentElement.children[2].textContent = 1;
+  })
+);
+
+document.querySelectorAll(".tombol-inc").forEach((inc) => {
+  inc.addEventListener("click", function () {
+    this.previousElementSibling.textContent =
+      parseInt(this.previousElementSibling.textContent) + 1;
+  });
+});
+document.querySelectorAll(".tombol-dec").forEach((dec) => {
+  dec.addEventListener("click", function () {
+    this.nextElementSibling.textContent -= 1;
+    if (this.nextElementSibling.textContent < 1) {
+      changeDisplay(this.parentElement.children);
+    }
+  });
+});
+
+function changeDisplay(children) {
+  Array.from(children).forEach((e) => e.classList.toggle("d-none"));
+}
