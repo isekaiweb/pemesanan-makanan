26983c555e17e18b9121c7143296edb62708a380
isekaiweb <cobasaja271@gmail.com>
2020-12-28 12:30:52 +0700

    fix total pemb

--- liff.js
+++ liff.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", () => {
+(function inisialiasiLiff() {
   const idLiff = "1655324717-zK2NJ5e3";
   liff
     .init({
@@ -10,7 +10,7 @@ window.addEventListener("load", () => {
     .catch((err) => {
       console.error(err);
     });
-});
+})();
 
 //cek apakah sudah login jika belum maka login
 function mulaiAPP() {
