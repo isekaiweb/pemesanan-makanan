bd1c07521d9266748441c442ae514208c8ec0372
isekaiweb <cobasaja271@gmail.com>
2020-12-28 20:30:03 +0700

    perbaikan alert

--- variabel.js
+++ variabel.js
@@ -18,7 +18,7 @@ const loading = document.querySelector(".load"),
 
 alertB4.classList = "alert alert-warning alert-dismissible fade show";
 alertB4.setAttribute("role", "alert");
-alertB4.innerHTML = `   <p class="text-center m-0"><strong>Anda mengakses Dikita diluar aplikasi LINE</strong>, akses  Dikita melalui Aplikasi <strong>LINE</strong> agar dapat menggunakan fitur Dikita secara penuh
+alertB4.innerHTML = `   <p class="text-center m-0"><strong>Anda mengakses Dikita diluar Aplikasi LINE</strong>, akses Dikita melalui Aplikasi <strong>LINE</strong> agar dapat menggunakan fitur Dikita secara penuh
                         </p>
                         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
